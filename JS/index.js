e3a460ff5c10a139be5a18d3c93fc1851d4b41c3
Alfonso Rafael Arcilla <alfonsorafaelarcilla@gmail.com>
2025-01-08 01:38:31 +0800

    added JS

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+console.log('Hello world');
+let name = 'nanashi';
+console.log(name);
+function cube(number){
+   return number*number*number;
+}
+console.log(cube(3));
+let setOfNumbers=[1, 2, 3];
+setOfNumbers[3]= 'gwapo';
+console.log(setOfNumbers); 
