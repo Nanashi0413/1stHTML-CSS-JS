8c96c27d9dcd0b44ec3db602bdffe2827e4f8edd
Alfonso Rafael Arcilla <alfonsorafaelarcilla@gmail.com>
2025-03-20 00:42:43 +0800

    nag praktis ko

--- JS/arrays-and-loops.js
+++ JS/arrays-and-loops.js
@@ -5,20 +5,37 @@ function renderTodoList () {
   let todoListHTML = '';
 
   for (let i = 0; i < todoArray.length; i++) {
-    const todo = todoArray[i];
-    const html = `<p>${todo}</p>`;
+    const todoObj = todoArray[i];
+    const {name, dueDate} = todoObj;
+    const html = `
+    <p>
+      ${name} ${dueDate}
+      <button 
+      onclick="
+        todoArray.splice(${i},1);
+        renderTodoList();
+      ">Delete</button>
+    </p>
+    `;
     todoListHTML += html;
-    document.querySelector('.js-todo-container')
-    .innerHTML = todoListHTML;
   }
+  document.querySelector('.js-todo-container')
+    .innerHTML = todoListHTML;
 }
 
 function addTodo () {
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
+  const dueDateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dueDateInputElement.value;
+
   
-  todoArray.push(name);
-  inputElement.value = ''
+  todoArray.push({
+    name,
+    dueDate
+  });
+  inputElement.value = '';
+  dueDateInputElement.value = '';
   renderTodoList();
 }
 
